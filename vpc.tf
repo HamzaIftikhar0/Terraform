7939dcf074b6d64089203a1af24365483b8edbdd
Hamza <iahamzaiftikhar2oo4@gmail.com>
2025-08-29 17:22:05 +0500

    final version

--- vpc.tf
+++ vpc.tf
@@ -7,7 +7,6 @@ module "vpc" {
   azs             = ["eu-east-2a", "eu-east-2b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
-
   enable_nat_gateway = true
   enable_vpn_gateway = true
 
